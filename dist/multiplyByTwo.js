c29e1cbb1a176a5a62bdc9ee66cc66e8c6c5fc74
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-04-13 11:15:40 +0800

    modify gitignore

--- dist/multiplyByTwo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var multiplyByTwo = function multiplyByTwo(numberToMultipy) {
-  return numberToMultipy * 2;
-};
-exports.default = multiplyByTwo;
